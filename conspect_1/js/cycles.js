7a986dfe581ec9f12c6dd59e396f1245c8714228
Ksenya Serakova <Afrota@yandex.ru>
2024-11-18 20:38:02 +0400

    циклы в цикле

--- conspect_1/js/cycles.js
+++ conspect_1/js/cycles.js
@@ -16,9 +16,53 @@ let num = 50;
 // должно состоять из трёх аргументов, но это не обязательно: dor (то, что будет в начале цикла; условие, при котором цикл остановит работу; шаг цикла (часто используется конструкция с инкриментом))
 for (let i = 1; i < 10; i++) {
     if (i % 2 == 0) {
-        // break; // прерыванмие цикла
+        // break; // прерывание цикла
         continue; // не прерывает цикл полностью, пропуская одну ненужную операцию
     }
 
     console.log(i);
+}
+
+
+// цикл в цикле и метки
+
+for (let i = 0; i < 3; i++) {
+    console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
+}
+
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+let result = '';
+const lenght = 7;
+
+for (let i = 1; i < lenght; i++) {
+
+    for (let j = 0; j < i; j++) {
+        result += "*";
+    }
+
+    result += '\n';
+}
+
+console.log(result);
+
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) continue first;
+            console.log(`Third level: ${k}`);
+        }
+    }
 }
