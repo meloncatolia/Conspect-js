b7a6195ff4c89a7d02448857b8b38d28f52cd001
Ksenya Serakova <Afrota@yandex.ru>
2025-02-05 16:40:07 +0400

    правила и типы названия переменных + нумерация файлов

---
+++ conspect_1/js/3variableNames.js
@@ -0,0 +1,22 @@
+const vehicleBodyWidth = 5000,
+      vehicleBodyLength = 4000;
+
+////
+
+console.log("Ширина кузова втомобиля: " + vehicleBodyWidth + ', длина: ' + vehicleBodyLength);
+
+// ['vruru', 'ecrvrev', 'vvuquegv'].map(a => )
+
+// Request
+// data
+// response
+
+
+
+// snake_case
+// UPPER_SNAKE_CASE - капсом обозначаются константы
+// Lebab-case
+// PascalCase - используется для названия классов в JS
+
+const COLOR_RED = '#F00'; // капсом обозначают константы
+
